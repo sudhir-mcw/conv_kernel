16af1371354cd5a1895697e2c06541ebf56b8340
Sudhir <sudhir.shanmugam@multicorewareinc.com>
2024-08-29 07:31:50 +0000

    update nhwc impl

---
+++ conv1x1/conv64x128x1x1_nhwc.py
@@ -0,0 +1,59 @@
+import numpy as np
+import torch
+from torch import nn
+
+
+CHANNELS = 128
+HEIGHT = 224
+WIDTH = 224
+KERNEL_HEIGHT = 1
+KERNEL_WIDTH = 1
+OUTPUT_CHANNELS = 64
+STRIDE = 2
+PADDING = 2
+
+if __name__ == "__main__":
+
+    input_matrix = [(i + 1) for i in range(0, (1 * CHANNELS * HEIGHT * WIDTH))]
+    input_tensor = torch.Tensor(input_matrix).reshape(1, CHANNELS, HEIGHT, WIDTH)
+
+    print("nchw input shape ", input_tensor.shape)
+    input_tensor_nhwc = input_tensor.permute(0, 2, 3, 1)  # for nhwc conversion
+    print("nhwc input shape ", input_tensor_nhwc.shape)
+    np.save("py_input_nhwc.npy", input_tensor_nhwc.detach().numpy())
+
+    weight_matrix = [
+        (i + 1)
+        for i in range(
+            0, (1 * OUTPUT_CHANNELS * CHANNELS * KERNEL_HEIGHT * KERNEL_WIDTH)
+        )
+    ]
+    weight_tensor = torch.Tensor(weight_matrix).reshape(
+        OUTPUT_CHANNELS, CHANNELS, KERNEL_HEIGHT, KERNEL_WIDTH
+    )
+
+    print("nchw weight shape ", weight_tensor.shape)
+    weight_tensor_nhwc = weight_tensor.permute(2, 3, 1, 0)  # for nhwc conversion
+    print("nhwc weight shape ", weight_tensor_nhwc.shape)
+    np.save("py_weight_nhwc.npy", weight_tensor_nhwc.detach().numpy())
+
+    bias_matrix = [(0) for i in range(0, (1 * OUTPUT_CHANNELS))]
+    bias_tensor = torch.Tensor(bias_matrix)
+
+    conv = nn.Conv2d(
+        OUTPUT_CHANNELS,
+        CHANNELS,
+        kernel_size=(KERNEL_HEIGHT, KERNEL_WIDTH),
+        stride=STRIDE,
+        padding=PADDING,
+    )
+    conv.weight.data = weight_tensor
+    conv.bias.data = bias_tensor
+
+    output_tensor = conv(input_tensor)
+
+    print("nchw output shape ", output_tensor.shape)
+    output_tensor_nhwc = output_tensor.permute(0, 2, 3, 1)  # for nhwc conversion
+
+    print("nhwc output shape ", output_tensor_nhwc.shape)
+    np.save("py_conv64x128x1x1_nhwc.npy", output_tensor_nhwc.detach().numpy())
--- conv1x1/test.sh
+++ conv1x1/test.sh
@@ -1,8 +1,10 @@
-rm *.bin *.npy conv64x128x1x1
-g++ conv64x128x1x1.cc -o conv64x128x1x1  -std=c++11 -lcnpy && ./conv64x128x1x1
-g++ conv64x128x1x1_nhwc.cc -o conv64x128x1x1_nhwc  -std=c++11 -lcnpy && ./conv64x128x1x1_nhwc
+rm *.bin *.npy *.o
+g++ conv64x128x1x1.cc -o conv64x128x1x1.o  -std=c++11 -lcnpy && ./conv64x128x1x1.o
+g++ conv64x128x1x1_nhwc.cc -o conv64x128x1x1_nhwc.o  -std=c++11 -lcnpy && ./conv64x128x1x1_nhwc.o
 /home/ubuntu/tvm_env/bin/python conv64x128x1x1.py
+/home/ubuntu/tvm_env/bin/python conv64x128x1x1_nhwc.py
 echo "NCHW"
 /home/ubuntu/tvm_env/bin/python compare.py conv64x128x1x1_nchw.npy py_conv64x128x1x1.npy
 echo "NHCW"
-/home/ubuntu/tvm_env/bin/python compare.py conv64x128x1x1_nhwc.npy py_conv64x128x1x1.npy
+/home/ubuntu/tvm_env/bin/python compare.py conv64x128x1x1_nhwc.npy py_conv64x128x1x1_nhwc.npy
+rm *.bin *.npy *.o
